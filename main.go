268e215c9c24707920e852035f8e29eaa4b9189e
Owais Javed <contact@owais.fi>
2026-04-20 12:23:59 +0300

    update argument parsing

--- main.go
+++ main.go
@@ -7,10 +7,40 @@ import (
 
 func main() {
 
-	if len(os.Args) != 4 || os.Args[1] == "help" {
+	if len(os.Args) != 4 || os.Args[1] == "help" || os.Args[1] == "-h" {
 		fmt.Println("itinerary usage:\n" +
 			"go run . ./input.txt ./output.txt ./airport-lookup.csv")
+		return
 	}
 
-	fmt.Println("Hello World")
+	inputPath := os.Args[1]
+		if _, err := os.Stat(inputPath); os.IsNotExist(err) {
+			fmt.Println("Input file not found!")
+			return
+		}
+
+	outputPath := os.Args[2]
+
+	lookupPath := os.Args[3]
+		if _, err := os.Stat(lookupPath); os.IsNotExist(err) {
+			fmt.Println("Airport lookup file not found!")
+			return
+		}
+
+	content, err := os.ReadFile(inputPath)
+		if err != nil {
+				//fmt.Println("Error in Reading Lookup File")
+			log.Fatal(err)
+		}
+
+	err = os.WriteFile("output.txt", []byte(content), 0644)
+
+		//reg := regexp.MustCompile("#[A-Z]{3}")
+
+		//matches := reg.FindAllString(content, -1)
+
+		//fmt.Println(matches)
+
+		//fmt.Println(string(content))
+	*/
 }
