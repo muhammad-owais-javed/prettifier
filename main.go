1d033995068b8d84fa860717e1eeca53a2fe15b3
Owais Javed <contact@owais.fi>
2026-04-21 15:20:23 +0300

    add date & time parsing

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -62,6 +64,110 @@ func main() {
 	for code, airportName := range icaoMap {
 		match := "##" + code
 		textContent = strings.ReplaceAll(textContent, match, airportName)
+	}
+
+	reg := regexp.MustCompile(`(D|T12|T24)\((.*?)\)`)
+	matches := reg.FindAllStringSubmatch(textContent, -1)
+	//fmt.Println(matches)
+
+	monthMap := map[string]string{
+		"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun",
+		"07": "Jul", "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec",
+	}
+
+	for _, match := range matches {
+
+		defaultDate := match[0]
+		dateTag := match[1]
+		//fmt.Println("dateTag: " + dateTag)
+		isoDate := match[2]
+
+		// To check if the format is not matched, skip it
+		if !strings.Contains(isoDate, "T") || !strings.Contains(isoDate, "+") && !strings.Contains(isoDate, "-") && !strings.Contains(isoDate, "Z") {
+			continue
+		}
+
+		parts := strings.Split(isoDate, "T")
+		//fmt.Println(parts)
+
+		if len(parts) != 2 {
+			continue
+		}
+
+		date := parts[0]
+		//fmt.Println("Date: " + date)
+		timeWoffset := parts[1]
+		//fmt.Println("Time & Offset: " + timeWoffset)
+
+		dateSplit := strings.Split(date, "-")
+		year := dateSplit[0]
+		month := dateSplit[1]
+		day := dateSplit[2]
+
+		//fmt.Println("Year: " + year + " Month: " + month + " Day: " + day)
+
+		var time string
+		var offset string
+
+		if strings.HasSuffix(timeWoffset, "Z") {
+			time = strings.TrimSuffix(timeWoffset, "Z")
+			offset = "+00:00"
+			//fmt.Println("Time: " + time + " Offset: " + offset)
+		} else if strings.Contains(timeWoffset, "+") {
+			toffSplit := strings.Split(timeWoffset, "+")
+			time = toffSplit[0]
+			offset = "+" + toffSplit[1]
+			//fmt.Println("Time: " + time + " Offset: " + offset)
+		} else if strings.Contains(timeWoffset, "-") {
+			toffSplit := strings.Split(timeWoffset, "-")
+			time = toffSplit[0]
+			offset = "-" + toffSplit[1]
+			//fmt.Println("Time: " + time + " Offset: " + offset)
+		} else {
+			//fmt.Println("No Offset Found")
+			continue
+		}
+
+		timeSplit := strings.Split(time, ":")
+		hoursStr := timeSplit[0]
+		minutes := timeSplit[1]
+		//seconds := timeSplit[2]
+
+		//fmt.Println("hoursStr: " + hoursStr + " Minutes: " + minutes + " Seconds: " + seconds)
+
+		var formatResult string
+
+		switch dateTag {
+		case "D":
+			//fmt.Println("Case D")
+			monthName, _ := monthMap[month]
+			formatResult = fmt.Sprintf("%s-%s-%s", day, monthName, year)
+
+		case "T12":
+			hours, err := strconv.Atoi(hoursStr)
+			if err != nil {
+				continue
+			}
+			AMPM := "AM"
+			if hours >= 12 {
+				AMPM = "PM"
+			}
+			if hours > 12 {
+				hours = hours - 12
+			}
+			if hours == 0 {
+				hours = 12
+			}
+			formatResult = fmt.Sprintf("%02d:%s:%s (%s)", hours, minutes, AMPM, offset)
+
+		case "T24":
+			//fmt.Println("Case T24")
+			formatResult = fmt.Sprintf("%s:%s (%s)", hoursStr, minutes, offset)
+			//fmt.Println("Case T24 formatResult: " + formatResult)
+		}
+
+		textContent = strings.Replace(textContent, defaultDate, formatResult, 1)
+		//	fmt.Println("Text Content: " + textContent)
 	}
 
 	processedContent := textContent
